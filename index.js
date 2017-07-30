1ddcc1e86d7a530693f8ccf834aea01e0ba12257
LitoMore <litomore@gmail.com>
2017-07-31 01:34:50 +0800

    add spinner and table

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const Fanfou = require('fanfou-sdk')
+const Table = require('cli-table2')
 const async = require('async')
 const chalk = require('chalk')
 const pangu = require('pangu')
+const ora = require('ora')
 
 const {
   CONSUMER_KEY,
@@ -45,10 +47,9 @@ const getFollowers = () => {
   })
 }
 
-const find1 = async () => {
+const find1 = async (spinner) => {
   const friends = await getFriends()
-  console.log('你关注了却没关注你的人：')
-
+  const list = []
   return new Promise((resolve, reject) => {
     async.eachLimit(friends, REQUEST_LIMIT, (id, callback) => {
       ff.get('/friendships/show', {
@@ -56,26 +57,37 @@ const find1 = async () => {
         target_login_name: id
       }, (err, res) => {
         if (err) {
-          console.log(`${chalk.bgRed.white(pangu.spacing(err.message))} ${chalk.blue(id)}`)
+          const name = pangu.spacing(err.message)
+          list.push({
+            type: 'error',
+            name,
+            id
+          })
+          spinner.text = `谁没关注你 - ${chalk.red(name)} ${chalk.blue(id)}`
           callback()
         } else {
           if (res.relationship.source.followed_by === 'false') {
-            console.log(`${chalk.green(res.relationship.target.screen_name)} ${chalk.blue(res.relationship.target.id)}`)
+            const name = res.relationship.target.screen_name
+            list.push({
+              type: 'user',
+              name,
+              id
+            })
+            spinner.text = `谁没关注你 - ${chalk.green(name)} ${chalk.blue(id)}`
           }
           callback()
         }
       })
     }, err => {
       if (err) reject(err)
-      else resolve()
+      else resolve(list)
     })
   })
 }
 
-const find2 = async () => {
+const find2 = async (spinner) => {
   const followers = await getFollowers()
-  console.log('\n关注了你你却没关注他的人：')
-
+  const list = []
   return new Promise((resolve, reject) => {
     async.eachLimit(followers, REQUEST_LIMIT, (id, callback) => {
       ff.get('/friendships/show', {
@@ -83,26 +95,72 @@ const find2 = async () => {
         target_login_name: id
       }, (err, res) => {
         if (err) {
-          console.log(`${chalk.bgRed.white(pangu.spacing(err.message))} ${chalk.blue(id)}`)
+          const name = pangu.spacing(err.message)
+          list.push({
+            type: 'error',
+            name,
+            id
+          })
+          spinner.text = `谁没关注你 - ${chalk.red(name)} ${chalk.blue(id)}`
           callback()
         } else {
           if (res.relationship.source.following === 'false') {
-            console.log(`${chalk.green(res.relationship.target.screen_name)} ${chalk.blue(res.relationship.target.id)}`)
+            const name = res.relationship.target.screen_name
+            list.push({
+              type: 'user',
+              name,
+              id
+            })
+            spinner.text = `你没关注谁 - ${chalk.green(name)} ${chalk.blue(id)}`
           }
           callback()
         }
       })
     }, err => {
       if (err) reject(err)
-      else resolve()
+      else resolve(list)
     })
   })
 }
 
 const run = async () => {
   await xauth()
-  await find1()
-  await find2()
+
+  const spinner1 = ora('谁没关注你').start()
+  const result1 = await find1(spinner1)
+  spinner1.succeed('谁没关注你 ' + chalk.green(`(${result1.length})`))
+
+  const spinner2 = ora('你没关注谁').start()
+  const result2 = await find2(spinner2)
+  spinner2.succeed('你没关注谁 ' + chalk.green(`(${result2.length})`))
+
+  // Draw Table
+  const table = new Table({
+    head: [chalk.white('谁没关注你'), chalk.white('你没关注谁')],
+    chars: {
+      'mid': '',
+      'left-mid': '',
+      'mid-mid': '',
+      'right-mid': ''
+    }
+  })
+  const lineCount = result1.length > result2.length ? result1.length : result2.length
+  for (let i = 0; i < lineCount; i++) {
+    const line = [
+      i < result1.length
+        ? `${result1[i].type === 'error'
+          ? chalk.bgRed(result1[i].name)
+          : chalk.green(result1[i].name)} ${chalk.blue(result1[i].id)}`
+        : '',
+      i < result2.length
+        ? `${result2[i].type === 'error'
+          ? chalk.bgRed(result2[i].name)
+          : chalk.green(result2[i].name)} ${chalk.blue(result2[i].id)}`
+        : ''
+    ]
+    table.push(line)
+  }
+  console.log(table.toString())
 }
 
 run()
